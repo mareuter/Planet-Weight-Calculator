583b681f843fea3224c9912f72c72cb15cb06d36
Michael Reuter <mareuternh@gmail.com>
2011-09-13 21:41:38 -0400

    Adding import to launcher script.

--- bin/planet_weight_calc.py
+++ bin/planet_weight_calc.py
@@ -1,6 +1,8 @@
 if __name__ == "__main__":
     import sys
+    from PyQt4 import QtGui
     import pwc
+
     app = QtGui.QApplication(sys.argv)
     pwcd = pwc.PlanetWeightCalculator()
     pwcd.show()
