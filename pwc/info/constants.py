dfdca8dac0a9280247ce739545977342a5550247
Michael Reuter <mareuternh@gmail.com>
2011-09-15 21:51:32 -0400

    Moving constants to new module. This refs #3.

---
+++ pwc/info/constants.py
@@ -0,0 +1,21 @@
+'''
+Created on Sep 15, 2011
+
+@author: Michael
+'''
+
+LBS_TO_KG = 0.45359237
+
+PLANETS = {
+    "sun": 27.94,
+    "moon": 0.167,
+    "mercury": 0.37,
+    "venus": 0.88,
+    "mars": 0.38,
+    "ceres": 0.0276,
+    "jupiter": 2.64,
+    "saturn": 1.15,
+    "uranus": 1.17,
+    "neptune": 1.18,
+    "pluto": 0.059
+    }
--- pwc/planet_weight_calculator.py
+++ pwc/planet_weight_calculator.py
@@ -1,20 +1,7 @@
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 import ui
-
-PLANETS = {
-    "sun": 27.94,
-    "moon": 0.167,
-    "mercury": 0.37,
-    "venus": 0.88,
-    "mars": 0.38,
-    "ceres": 0.0276,
-    "jupiter": 2.64,
-    "saturn": 1.15,
-    "uranus": 1.17,
-    "neptune": 1.18,
-    "pluto": 0.059
-    }
+import info
 
 class PlanetWeightCalculator(QtGui.QDialog, ui.Ui_PlanetWeightDialog):
     def __init__(self, parent=None):
@@ -58,8 +45,8 @@ class PlanetWeightCalculator(QtGui.QDialog, ui.Ui_PlanetWeightDialog):
         return self.__weight * factor
 
     def _writeCalc(self):
-        for key in PLANETS:
-            value = self._calcWeight(PLANETS[key])
+        for key in info.PLANETS:
+            value = self._calcWeight(info.PLANETS[key])
             le = getattr(self, key+"LineEdit")
             le.setText("%.1f %s" % (value, self.__units))
             
